cf439de126be73a2674e59f2b0388198bd04cf41
Sayan Chowdhury <sayan.chowdhury.aiml25@heritageit.edu.in>
2022-05-25 00:45:10 +0530

    Added bars

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,18 @@ import React from "react";
 import { defaultImgs } from "../defaultimgs";
 import { Icon, TextArea } from "web3uikit";
 import "./Home.css";
+import { useState, useRef } from "react";
 
 const Home = () => {
+  const inputFile = useRef(null);
+  const [selectedFile, setSelectedFile] = useState();
+  const onImageClick = () => {
+    inputFile.current.click();
+  };
+  const changeHandler = (event) => {
+    const img = event.target.file[0];
+    setSelectedFile(URL.createObjectURL(img));
+  };
   return (
     <>
       <div className="mainContent">
@@ -17,11 +27,26 @@ const Home = () => {
               type="text"
               width="95%"
             ></TextArea>
+            {selectedFile && (
+              <img src={selectedFile} className="tweetImg"></img>
+            )}
             <div className="imgorTweet">
-              <div className="imgDiv">
+              <div className="imgDiv" onClick={onImageClick}>
+                <input
+                  type="file"
+                  name="file"
+                  ref={inputFile}
+                  // onChange={changeHandler}
+                  state={{ display: "none" }}
+                />
                 <Icon fill="#1DA1F2" size={20} svg="image"></Icon>
               </div>
-              <div className="tweetOptions"></div>
+              <div className="tweetOptions">
+                <div className="tweet">Tweet</div>
+                <div className="tweet" style={{ backgroundColor: "#8247e5" }}>
+                  <Icon fill="#ffffff" size={20} svg="matic" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
