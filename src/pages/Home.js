ada0bd18c7c6e3d9a35ca59c4973dbf8c90c0f7e
Sayan Chowdhury <sayan.chowdhury.aiml25@heritageit.edu.in>
2022-05-24 20:41:02 +0530

    Improved UI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { defaultImgs } from "../defaultimgs";
+import { Icon, TextArea } from "web3uikit";
 import "./Home.css";
 
 const Home = () => {
   return (
     <>
-      <Link to="/">
-        <div>Home</div>
-      </Link>
-      <Link to="/profile">
-        <div>Profile</div>
-      </Link>
-
-      <Link to="/settings">
-        <div>Settings</div>
-      </Link>
+      <div className="mainContent">
+        <div className="profileTweet">
+          <img src={defaultImgs[0]} className="profilePic"></img>
+          <div className="tweetBox">
+            <TextArea
+              label=""
+              name="tweetTxtArea"
+              value="GM"
+              type="text"
+              width="95%"
+            ></TextArea>
+            <div className="imgorTweet">
+              <div className="imgDiv">
+                <Icon fill="#1DA1F2" size={20} svg="image"></Icon>
+              </div>
+              <div className="tweetOptions"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
